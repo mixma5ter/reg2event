3d578ba795d6b5b10955827670b88af3c66f913c
Евгений Спиридонов <deejay999@yandex.ru>
2023-05-19 09:38:22 +0300

    add forms model

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import RegForm
+
+
+class MyAdmin(admin.ModelAdmin):
+    """Переопределение общих параметров для всех моделей админки."""
+
+    save_on_top = True
+    list_select_related = True
+    empty_value_display = '-пусто-'
+
+    class Meta:
+        abstract = True
+
+
+@admin.register(RegForm)
+class FormAdmin(MyAdmin):
+    """Регистрация модели конкурса в админке."""
+
+    list_display = (
+        'pk',
+        'deal_id',
+        'author',
+        'title',
+        'fields',
+        'event_date',
+        'pub_date',
+        'update_date',
+    )
+    list_display_links = ('deal_id', 'title',)
+    search_fields = ('deal_id', 'title',)
+    list_filter = ('author', 'event_date',)
--- forms/models.py
+++ forms/models.py
@@ -1,3 +1,56 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+User = get_user_model()
+
+
+def getUser():
+    """Callable function."""
+    return User.objects.get_or_create(username=User)
+
+
+class RegForm(models.Model):
+    """Модель формы регистрации на мероприятие."""
+
+    deal_id = models.IntegerField(
+        db_index=True,
+        verbose_name='ID мероприятия',
+        help_text='Введите ID мероприятия'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.SET(getUser),
+        related_name='posts',
+        verbose_name='Автор'
+    )
+    title = models.CharField(
+        max_length=255,
+        verbose_name='Название мероприятия',
+        help_text='Введите название мероприятия',
+    )
+    fields = models.TextField(
+        blank=True,
+        verbose_name='Список полей',
+        help_text='Введите список полей через запятую',
+    )
+    event_date = models.DateField(
+        verbose_name='Дата мероприятия',
+        help_text='Выберите дату мероприятия',
+    )
+    pub_date = models.DateTimeField(
+        db_index=True,
+        auto_now_add=True,
+        verbose_name='Дата создания',
+    )
+    update_date = models.DateTimeField(
+        auto_now=True,
+        verbose_name='Дата изменения',
+    )
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Форма регистрации'
+        verbose_name_plural = 'Формы регистрации'
+
+    def __str__(self):
+        return self.title
