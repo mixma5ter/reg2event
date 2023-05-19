afbc4c667e70d25d4466164b1aef242093be215c
Евгений Спиридонов <deejay999@yandex.ru>
2023-05-19 10:24:50 +0300

    edit index page

--- forms/urls.py
+++ forms/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 
-from . import views
+from .views import IndexView
 
 app_name = 'forms'
 
 urlpatterns = [
     # Главная страница
-    path('', views.index, name='index'),
-    # Список форм
-    # path('forms/', views.forms_list, name='forms_list'),
-    # Просмотр и редактирование формы
-    # path('forms/<int:form_id>/', views.form_detail, name='form_detail'),
-    # Добавление новой формы
-    # path('create/', views.create_form, name='create_form'),
+    path('', IndexView.as_view(), name='index'),
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,16 +1,25 @@
 from django.shortcuts import render, redirect
+from django.views.generic import DetailView, ListView
+
+from .models import RegForm
+
+CARDS_ON_PAGE = 6
 
 
-def index(request):
+class IndexView(ListView):
     """Главная страница."""
 
-    if request.user.is_authenticated:
-        title = 'Регистрация обращений'
-        template_name = 'forms/index.html'
-        context = {
-            'user': request.user,
-            'title': title,
-        }
-        return render(request, template_name, context)
+    template_name = 'forms/index.html'
+    context_object_name = 'forms'
+    title = 'Регистрация обращений'
 
-    return redirect('login')
+    def get_queryset(self):
+        cards = RegForm.objects.all().order_by('-pub_date')[:CARDS_ON_PAGE]
+        return cards
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user'] = self.request.user
+        context['title'] = self.title
+        context['cards'] = self.get_queryset()
+        return context
