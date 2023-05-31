2b7076deeed810b7dc47cb1f5f889ef72d6a09b1
Евгений Спиридонов <deejay999@yandex.ru>
2023-05-31 09:35:33 +0300

    add test data command

---
+++ core/management/commands/add_test_data.py
@@ -0,0 +1,44 @@
+import random
+import uuid
+
+from django.core.management import BaseCommand
+
+from forms.models import Form, Field, User
+
+BASIC_FIELDS = [
+    {'ФИО': 'text'},
+    {'Организация': 'text'},
+    {'Должность': 'text'},
+    {'Почта': 'email'},
+    {'Телефон': 'phone'},
+    {'Комментарий': 'textarea'}
+]
+
+
+class Command(BaseCommand):
+    help = 'Создает несколько тестовых форм с базовыми полями.'
+
+    def handle(self, *args, **options):
+        template = 'Тестовая регистрация на мероприятие'
+        uploaded = 0
+
+        for _ in range(8):
+            try:
+                Form.objects.get_or_create(
+                    deal_id=str(uuid.uuid1().int)[:random.randrange(4, 6)],
+                    author=random.choice(User.objects.all()),
+                    title=template,
+                )
+                form_id = Form.objects.latest('id').pk
+                fields = [
+                    Field(label=label, field_type=field_type, form_id=form_id,
+                          is_active=random.choice(['True', 'False'])) for
+                    field_data in BASIC_FIELDS for label, field_type in field_data.items()]
+                Field.objects.bulk_create(fields)
+                uploaded += 1
+            except:
+                pass
+
+        message = f'\nЗагружено - {uploaded} форм.'
+
+        self.stdout.write(self.style.SUCCESS(message))
