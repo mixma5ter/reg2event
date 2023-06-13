0e2e38d6f5f44af877457aa1951912eb2be39c3f
Евгений Спиридонов <deejay999@yandex.ru>
2023-06-13 12:45:56 +0300

    delete name validation

--- static/js/includes/form_validation.js
+++ static/js/includes/form_validation.js
@@ -1,15 +1,3 @@
-// проверка поля ФИО на наличие только букв и пробелов
-$(document).ready(function () {
-    $('#id_ФИО').on('input', function () {
-        var regex = /[^a-zA-Zа-яА-Я\s-ё]/g;
-        if ($(this).val().match(regex)) {
-            $(this).addClass('is-invalid');
-        } else {
-            $(this).removeClass('is-invalid');
-        }
-    });
-});
-
 // проверяет, что в поле телефона введены только цифры и знак плюса,
 // а также что количество символов в номере телефона от 10 до 14
 $(document).ready(function () {
