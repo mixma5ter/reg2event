9cbbf76c8bb4eabd52778082095574ba07a8ecd7
Евгений Спиридонов <deejay999@yandex.ru>
2023-06-26 16:19:17 +0300

    urls changes

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('users.urls', namespace='users')),
-    path('auth/', include('django.contrib.auth.urls')),
-    path('forms/', include('forms.urls', namespace='forms')),
-    path('reg/', include('reg.urls', namespace='reg')),
+    path('events/admin/', admin.site.urls),
+    path('events/auth/', include('users.urls', namespace='users')),
+    path('events/auth/', include('django.contrib.auth.urls')),
+    path('events/forms/', include('forms.urls', namespace='forms')),
+    path('events/reg/', include('reg.urls', namespace='reg')),
 ]
 
 # Обработчики страниц ошибок
