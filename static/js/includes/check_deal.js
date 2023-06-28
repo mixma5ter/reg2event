d3b4849b849c400fc718b572e7eaef7a4fc3b0e1
Евгений Спиридонов <deejay999@yandex.ru>
2023-06-28 09:23:28 +0300

    redesign alert messages

--- static/js/includes/check_deal.js
+++ static/js/includes/check_deal.js
@@ -1,6 +1,7 @@
 let checkButton = document.getElementById('check-deal');
 let dealInput = document.getElementById('id_deal_id');
 let titleInput = document.getElementById('id_title');
+let alertMessages = document.getElementById('alert-messages');
 
 checkButton.addEventListener('click', function () {
     let dealId = dealInput.value;
@@ -9,11 +10,26 @@ checkButton.addEventListener('click', function () {
     fetch(`/events/forms/check_deal/${dealId}/`)
         .then(response => response.json())
         .then(data => {
+            // создаем элемент div для сообщения
+            let messageDiv = document.createElement('div');
+
             if (data.value) {
+                // заполняем поле title
                 titleInput.value = data.message;
+
+                messageDiv.classList.add('alert', 'alert-success');
+                messageDiv.textContent = data.message;
             } else {
-                alert(data.message);
+                messageDiv.classList.add('alert', 'alert-danger');
+                messageDiv.textContent = data.message;
             }
+            // добавляем сообщение в div с id "alert-messages"
+            alertMessages.appendChild(messageDiv);
+
+            // удаляем сообщение через 3 секунды
+            setTimeout(function () {
+                messageDiv.remove();
+            }, 3000);
         })
         .catch(err => console.error(err));
 });
