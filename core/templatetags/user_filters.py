6568d5cd4d676a1f806543e3fb0ca77cea94f9be
Евгений Спиридонов <deejay999@yandex.ru>
2023-11-02 14:18:16 +0300

    add user filter

--- core/templatetags/user_filters.py
+++ core/templatetags/user_filters.py
@@ -1,7 +1,13 @@
 from django import template
+
 register = template.Library()
 
 
 @register.filter
 def addclass(field, css):
     return field.as_widget(attrs={'class': css})
+
+
+@register.filter
+def not_in(value, arg):
+    return value not in arg.split(',')
